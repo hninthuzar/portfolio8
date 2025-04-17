1a19aa50907adaf33143c6d21838c8400272f9a1
hninthuzar <hninthuzar.itworld@gmail.com>
2025-04-17 18:24:51 +0630

    upload

---
+++ language.js
@@ -0,0 +1,63 @@
+export const dictionary = {
+    lang: {
+      en: 'English',
+      my: 'မြန်မာ'
+    },
+    hi: {
+        en: "Hi",
+        my: "ဟိုင်း"
+    },
+    name: {
+        en: 'I am Hnin Thu Zar',
+        my: 'နှင်းသူဇာ'
+    },
+    about_me: {
+        en: 'About Me',
+        my: 'ကိုယ့်အကြောင်း'
+    },
+    my_work: {
+        en: 'My Work',
+        my: 'အလုပ်အကြောင်း'
+    },
+    skills: {
+        en: "Skills",
+        my: "ကျွမ်းကျင်မှုများ"
+    },
+    contact: {
+        en: "Contact",
+        my: "ဆက်သွယ်ရန်"
+    },
+    view_resume: {
+        en: "View Resume",
+        my: "Resume ကိုကြည့်ပါ "
+    },
+    view_projects: {
+        en: "View All Projects",
+        my: "Projectများကိုကြည့်ပါ "
+    },
+    about_description: {
+        en: `I'm Zara — a senior front-end developer with a design-minded approach to building elegant, intuitive web interfaces.
+            <br><br>
+            With a strong foundation in JavaScript, modern frameworks, and responsive design, I specialize in crafting user experiences that are not only performant and accessible, but also visually polished and delightful to interact with.
+            <br><br>
+            I love bridging the gap between design and development — translating pixels into clean, scalable code and bringing life to UI through thoughtful interactions. Whether it's architecting a complex frontend system or fine-tuning microinteractions, I’m all about making the web feel beautiful and seamless.`,
+        my: `
+ကျွန်ုပ်သည် Zara ဖြစ်ပါသည် — a senior front-end developer with a design-minded approach to building elegant, intuitive web interfaces.
+            <br><br>
+            With a strong foundation in JavaScript, modern frameworks, and responsive design, I specialize in crafting user experiences that are not only performant and accessible, but also visually polished and delightful to interact with.
+            <br><br>
+            I love bridging the gap between design and development — translating pixels into clean, scalable code and bringing life to UI through thoughtful interactions. Whether it's architecting a complex frontend system or fine-tuning microinteractions, I’m all about making the web feel beautiful and seamless.`
+    },
+    skill_description: {
+        en: "These skill levels represent my current progress, but I'm always pushing to grow and achieve more.",
+        my: "ဤကျွမ်းကျင်မှုအဆင့်များသည် ကျွန်ုပ်၏လက်ရှိတိုးတက်မှုကို ကိုယ်စားပြုသော်လည်း ပိုမိုတိုးတက်အောင်မြင်ရန် အမြဲကြိုးစားနေပါသည်။ "
+    },
+    exp_description: {
+        en: "I have nearly 6 years of working experience in both solo and collaborative projects, applying my skills to real-world challenges. Each project has contributed to my professional growth, sharpened my problem-solving abilities, and boosted my confidence as a developer.",
+        my: "ကျွန်ုပ်တွင် တစ်ယောက်တည်း နှင့် ပူးပေါင်းဆောင်ရွက်သည့် ပရောဂျက်နှစ်ခုလုံးတွင် အလုပ်အတွေ့အကြုံ 6 နှစ်နီးပါးရှိပြီး ကျွန်ုပ်၏အရည်အချင်းများကို လက်တွေ့ကမ္ဘာ၏စိန်ခေါ်မှုများကိုအသုံးချပါ။ ပရောဂျက်တစ်ခုစီသည် ကျွန်ုပ်၏ပရော်ဖက်ရှင်နယ်တိုးတက်မှုကို ပံ့ပိုးပေးသည်၊ ကျွန်ုပ်၏ပြဿနာဖြေရှင်းနိုင်စွမ်းကို ထက်မြက်စေကာ developer တစ်ဦးအနေဖြင့် ကျွန်ုပ်၏ယုံကြည်မှုကို မြှင့်တင်ပေးပါသည်။ "
+    },
+    contact_description: {
+        en: "Get in touch with me for more information. If you need help or have question, I'm here for u.",
+        my: "မင်းအကူအညီလိုရင် ဒါမှမဟုတ် မေးစရာရှိရင်  ဆက်သွယ်နိုင်ပါသည်။ "
+    },
+  };
